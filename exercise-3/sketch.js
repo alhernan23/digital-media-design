8af06b4cd60a120287ede0703763a9f061e0dfc2
alhernan23 <amhernandez20@dons.usfca.edu>
2023-04-05 11:04:54 -0700

    hw experiment

--- exercise-3/sketch.js
+++ exercise-3/sketch.js
@@ -1,11 +1,13 @@
 //GLOBAL VARIABLES HERE AT THE TOP
 let input, button, result, question, submit, answer;
 
-let c;
+// let c;
 
 let Answer1;
 
 let colorInput = document.getElementById('color-input');
+
+let yesNo = document.getElementById('yes-no');
 
 function setup() {
     // make the div called "mySketch" a p5.js canvas element
@@ -14,7 +16,7 @@ function setup() {
 
 
     //TYPE A COLOR RESPONSE
-    result = createElement('p', 'type a color');
+    result = createElement('p', 'type "blue"');
     result.style('font-size', '30px');
     result.style('margin', '0px');
     result.style('text-align', 'center');
@@ -73,30 +75,31 @@ function output() {
 
   if (answers == "blue"){ //can change it to accomodate with case sensitivity 
     background(0,0,255);
-    Answer1.innerHTML = answers;
+    Answer1.innerHTML = "very good. you are blue, and only blue";
     //a link will appear for the next page
-    let a = createA('next.html', 'next');
+    let a = createA('page2.html', 'next');
     a.id('next');
     colorInput.appendChild(next);
-    c = color(230, 66, 245);
-    circles();
-  } else if (answers == "red"){
-    background(255,0,0);
-    Answer1.innerHTML = answers;
-  } else if (answers == "green"){
-    background(0,255,0);
-    c = color(255, 220, 66);
-    circles();
-    Answer1.innerHTML = answers;
-  } else if (answers == "magenta"){
-    background(255,0,255);
-    Answer1.innerHTML == answers;
+    // c = color(230, 66, 245);
+    // circles();
+  // } else if (answers == "red"){
+  //   background(255,0,0);
+  //   Answer1.innerHTML = answers;
+  // } else if (answers == "green"){
+  //   background(0,255,0);
+  //   // c = color(255, 220, 66);
+  //   // circles();
+  //   Answer1.innerHTML = answers;
+  // } else if (answers == "magenta"){
+  //   background(255,0,255);
+  //   Answer1.innerHTML == answers;
   } else {
-    Answer1.innerHTML = "i don't know that color";
+    background(255,0,0);
+    Answer1.innerHTML = "HOW DARE YOU. TYPE IN ONLY THE COLOR BLUE, NO OTHER COLOR";
   }
 }
 
-function circles(){
-  fill(c);
-  ellipse(200,200,200,200);
-}
+// function circles(){
+//   fill(c);
+//   ellipse(200,200,200,200);
+// }
