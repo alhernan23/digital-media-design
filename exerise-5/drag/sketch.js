330afe2e69b1d5573b7e11cb0adff5a860ce194a
alhernan23 <amhernandez20@dons.usfca.edu>
2023-04-17 10:38:17 -0700

    exercise 5 part 2

--- exerise-5/drag/sketch.js
+++ exerise-5/drag/sketch.js
@@ -7,18 +7,29 @@ function setup() {
 
     //as long as you add the class "dragme" to the image, it should work.
 
-    iris = createImg('images/iris.png')
-    iris.position(700, 300);
-    iris.class('dragme')
+    // iris = createImg('images/iris.png')
+    // iris.position(700, 300);
+    // iris.class('dragme')
 
-    violet = createImg('images/violet.png')
-    violet.position(700, 300);
-    violet.class('dragme')
+    // violet = createImg('images/violet.png')
+    // violet.position(700, 300);
+    // violet.class('dragme')
 
-    jasmine = createImg('images/jasmine.png')
-    jasmine.position(700, 300);
-    jasmine.class('dragme')
+    // jasmine = createImg('images/jasmine.png')
+    // jasmine.position(700, 300);
+    // jasmine.class('dragme')
 
+  yellow = createImg ('images/yellow.png')
+  yellow.position(300,300);
+  yellow.class('dragme')
+
+  green = createImg ('images/green.png')
+  green.position(700,300);
+  green.class('dragme')
+
+  blue = createImg ('images/blue.png')
+  blue.position(1100, 300);
+  blue.class('dragme')
 
   }
   
