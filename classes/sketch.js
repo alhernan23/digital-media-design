dc1397a840be5d82b2a55e73d8403ee3b890f592
alhernan23 <amhernandez20@dons.usfca.edu>
2023-04-24 10:11:21 -0700

    class exercise

--- classes/sketch.js
+++ classes/sketch.js
@@ -1,13 +1,13 @@
 
-let bubbles = [];
+let bubbles = []; //array that holds everything
 
 let lamb;
-let flowers = [];
+let flowers = []; //array that holds flower imgs
 
-function preload() {
+function preload() { //loads to be used later; doesn't make them appear just yet
   lamb = loadImage('images/lamb.png');
-  for (let i = 0; i < 3; i++) {
-    flowers[i] = loadImage(`images/flower${i}.png`);
+  for (let i = 0; i < 3; i++) { //repeats an action a certain amt of times
+    flowers[i] = loadImage(`images/flower${i}.png`); //{i} reprsents the number; label your img correctly!
   }
 }
 
@@ -16,6 +16,7 @@ function setup() {
   let cnv = createCanvas(windowWidth, windowHeight);
   cnv.parent('mySketch');
 
+  //populating the page (50 is the amt of objects on page), pushses it to the array
   for (let i = 0; i < 50; i++) {
     let x = random(width);
     let y = random(height);
@@ -42,15 +43,16 @@ function draw() {
   }
 }
 
-class Bubble {
-  constructor(x, y, r, img) {
+//CLASS!!!!!
+class Bubble { //group of flowers
+  constructor(x, y, r, img) { //wow a skeleton blueprint <3
     this.x = x;
     this.y = y;
     this.r = r;
-    this.flower = random(flowers);
+    this.flower = random(flowers); //randomize flower img
   }
 
-  clicked(px, py) {
+  clicked(px, py) { //METHOD
     //let d = dist(px, py, this.x, this.y);
     //if (d < this.r) {
     if (
@@ -60,11 +62,11 @@ class Bubble {
       py > this.y-this.r/2 &&
       py < this.y + this.r/2
     ) {
-      this.flower = lamb; //random(flowers);
+      this.flower = lamb; //random(flowers); //change img to lamb
     }
   }
 
-  hover(px,py){
+  hover(px,py){ //METHOD
     if (
       //look for the area the image is considering image mode being centered
       px > this.x-this.r/2 &&
@@ -73,31 +75,36 @@ class Bubble {
       py < this.y + this.r/2
     ) {
       cursor('grab');
+      this.x = this.x + random(-2, 2);
+    this.y = this.y + random(-2, 2);
     }
   }
 
-  move() {
+  move() { //METHOD
     // this.x = this.x + random(-2, 2);
     // this.y = this.y + random(-2, 2);
+    // ^ shaking
 
     //rotate based on cursor
-    let angle = atan2(mouseY - this.y, mouseX - this.x);
-    rotate(angle);
+    // let angle = atan2(mouseY - this.y, mouseX - this.x);
+    // rotate(angle);
   }
 
-  show() {
+  show() { //METHOD
 
-    //image(this.flower, this.x , this.y, this.r, this.r);
+    image(this.flower, this.x , this.y, this.r, this.r);
     
-    push();
-    translate(this.x, this.y);
-    this.move();
-    imageMode(CENTER);
-    image(this.flower, 0 , 0, this.r, this.r);
-    pop();
+    // push();
+    // translate(this.x, this.y);
+    // this.move();
+    // imageMode(CENTER);
+    // image(this.flower, 0 , 0, this.r, this.r);
+    // pop();
   }
 
 }
+
+
 
 function windowResized() {
   // resize the canvas when the screen is resized.
